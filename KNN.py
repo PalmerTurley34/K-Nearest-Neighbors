24ab733d2ae8b00d634c20b73cd5b0941489952f
Palmer Turley <palmerturley34@gmail.com>
2020-10-17 11:18:22 -0700

    first pass algorithm

---
+++ KNN.py
@@ -0,0 +1,42 @@
+class KNearestNeighbors:
+    class KNearestNeighbors:
+    import numpy as np
+    def __init__(self, n_neighbors):
+        self.k = n_neighbors
+
+    def fit(self, x, y):
+        self.x = np.array(x)
+        self.y = np.array(y)
+        self.n_classes = len(np.unique(self.y))
+        # encode labels
+        labels = np.unique(self.y)
+        self.i_to_label = {i: label for i, label in enumerate(labels)}
+        self.label_to_i = {label: i for i, label in enumerate(labels)}
+        self.y_enc = np.array([self.label_to_i[label] for label in self.y])
+
+    def single_predict(self, pred):
+        # search through the training data and find the distance from each point to the target
+        distances = []
+        for observation in self.x:
+            distances.append(np.linalg.norm(observation-pred))
+              
+        # look up the target values for the k nearest neighbors
+        targets = list(zip(distances, self.y_enc))
+        targets.sort(key=lambda x: x[0])
+        # y_pred = target class with most neighbors
+        k_nearest = targets[:self.k]
+        votes = [0] * self.n_classes
+        for vote in k_nearest:
+            votes[vote[1]]+=1
+        top_vote = votes.index(max(votes))
+        return self.i_to_label[top_vote]
+    
+    def predict(self, pred):
+        pred = np.array(pred)
+        if pred.ndim == 1:
+            return self.single_predict(pred)
+        elif pred.ndim == 2:
+            prediction = np.array([self.single_predict(x) for x in pred])
+            return prediction
+    
+        
