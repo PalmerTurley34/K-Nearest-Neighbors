b8b353ec36938466bb1ea66867c7bc4ebceca606
Palmer Turley <palmerturley34@gmail.com>
2020-10-20 20:09:39 -0700

    update readme

--- KNN.py
+++ KNN.py
@@ -1,19 +1,17 @@
 class KNearestNeighbors:
     import numpy as np
-    def __init__(self, n_neighbors=5, output='classification'):
+    def __init__(self, n_neighbors=5):
         self.k = n_neighbors
-        self.output = output
 
     def fit(self, x, y):
         self.x = np.array(x)
         self.y = np.array(y)
-        if self.output == 'classification':
-            self.n_classes = len(np.unique(self.y))
-            # encode labels
-            labels = np.unique(self.y)
-            self.i_to_label = {i: label for i, label in enumerate(labels)}
-            self.label_to_i = {label: i for i, label in enumerate(labels)}
-            self.y_enc = np.array([self.label_to_i[label] for label in self.y])
+        self.n_classes = len(np.unique(self.y))
+        # encode labels
+        labels = np.unique(self.y)
+        self.i_to_label = {i: label for i, label in enumerate(labels)}
+        self.label_to_i = {label: i for i, label in enumerate(labels)}
+        self.y_enc = np.array([self.label_to_i[label] for label in self.y])
 
     def single_predict(self, pred):
         # search through the training data and find the distance from each point to the target
@@ -26,14 +24,11 @@ class KNearestNeighbors:
         targets.sort(key=lambda x: x[0])
         # y_pred = target class with most neighbors
         k_nearest = targets[:self.k]
-        if self.output == 'classification':
-            votes = [0] * self.n_classes
-            for vote in k_nearest:
-                votes[vote[1]]+=1
-            top_vote = votes.index(max(votes))
-            return self.i_to_label[top_vote]
-        elif self.output == 'regression':
-            return np.mean(k_nearest)
+        votes = [0] * self.n_classes
+        for vote in k_nearest:
+            votes[vote[1]]+=1
+        top_vote = votes.index(max(votes))
+        return self.i_to_label[top_vote]
     
     def predict(self, pred):
         pred = np.array(pred)
--- README.md
+++ README.md
@@ -1,12 +1,13 @@
 # K-Nearest-Neighbors
 This is a simple Nearest Neighbors algorithm built on regular python and a little bit of NumPy.
 
-This algorithm can be used for both classification or regression problems, although, the default is set to classification.
+This algorithm is used for classification problems. It is a simple, brute-force model so 
+it will take a while to run for larger data sets. Compared to sci-kit learn's nearest neighbors classifier, it does very well. Tested on two small data sets and the output for all of the test data was exactly the same.
 
 To use this algorithm, instatiate the class with the number of neighbors you want to use to predict the output.
 
-Such as: '''neigh = KNearestNeighbors(5)''' 
+Such as: '''neigh = KNearestNeighbors(n_neighbors=5)'''
 
-For a regression problem, set the output as regression:
+No need to encode the class labels, the model will do that for.
+It will return the class label names in the prediction as well.
 
-'''neigh = KnearestNeighbors(n_neighbors=7, output='regression')'''
